cc01855e0ab221302d7134f6df7e5863d1396873
Joseph Walker <jwalker@rentpath.com>
2021-10-07 13:09:13 -0500

    Add review dates

--- src/lib/components/PullRequests/SummaryLines/Reviews.svelte
+++ src/lib/components/PullRequests/SummaryLines/Reviews.svelte
@@ -1,7 +1,21 @@
+<script lang="ts" context="module">
+	import { browser } from "$app/env";
+	import { writable } from "svelte/store";
+
+	const now = writable(new Date());
+
+	if (browser) {
+		setInterval(function () {
+			now.set(new Date());
+		}, 60000); // 1 Minute
+	}
+</script>
+
 <script lang="ts">
 	import type { Option } from "fp-ts/lib/Option.js";
 	import { identity } from "fp-ts/lib/function.js";
 	import { map, match } from "fp-ts/lib/Option.js";
+	import { intervalToDuration } from "date-fns"
 
 	import type { PullRequestQuery } from "$lib/generated/graphql";
 	import LineSkeleton from "$lib/components/Skeletons/LineSkeleton.svelte";
@@ -11,6 +25,30 @@
 
 	export let me: string;
 	export let reviews: Option<Review[]>;
+
+	function formatDistanceShort(now: Date, updatedAtRaw: any) {
+		const updatedAt = new Date(updatedAtRaw);
+		const delta = intervalToDuration({
+			start: updatedAt,
+			end: now
+		});
+
+		if (delta.years > 0) {
+			return `>${delta.years}y`;
+		} else if (delta.months > 0) {
+			return `>${delta.months}m`;
+		} else if (delta.weeks > 0) {
+			return `${delta.weeks}w`;
+		} else if (delta.days > 0) {
+			return `${delta.days}d`;
+		} else if (delta.hours > 0) {
+			return `${delta.hours}h`;
+		} else if (delta.minutes > 0) {
+			return `${delta.minutes}m`;
+		} else {
+			return `<1m`;
+		}
+	}
 
 	const numReviews = map((reviews: Review[]) => reviews.length);
 
@@ -36,7 +74,7 @@
 						<img class="icon not-approved" src="/close.svg" alt="not-approved" />
 					{/if}
 					<em>
-						{review.author.login}
+						{review.author.login}<span class="time">&nbsp;-&nbsp;{formatDistanceShort($now, review.updatedAt)}</span>
 					</em>
 				</a>
 			</li>
@@ -76,6 +114,8 @@
 	.reviews li a em {
 		border-left: 1px solid var(--border-color);
 		padding: 0 6px;
+		display: flex;
+		align-items: center;
 	}
 
 	.icon.approved {
@@ -93,5 +133,11 @@
 	.me a {
 		font-weight: var(--weight-bold);
 		color: #2e86de;
+	}
+
+	.time {
+		font-size: 0.7rem;
+		font-weight: bold;
+		color: #9d9d9d;
 	}
 </style>
